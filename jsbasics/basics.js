5a79fa06670373f6080009c3dfc9c2d0250beb10
roleson <rolesonjg96@gmail.com>
2023-08-03 20:21:51 +0530

    i am committed..

---
+++ jsbasics/basics.js
@@ -0,0 +1,4 @@
+// console.log("hello bro how are you");
+// let a = 10;
+// let b = 22;
+// console.log(a % b);
---
+++ jsbasics/loop.js
@@ -0,0 +1,5 @@
+let roleson = "i am roleson";
+let x = ['sdf', 'sdfs'];
+for (let i = 0; i < Array.length + 1; i++) {
+    console.log(x[i]);
+}
