e46b07dc0078e34f33b3afeffbf6e0a1718c8823
rolesonjg <114156421+rolesonjg@users.noreply.github.com>
2023-11-24 22:58:11 +0530

    Delete jsbasics directory

--- jsbasics/basics.js
+++
@@ -1,4 +0,0 @@
-// console.log("hello bro how are you");
-// let a = 10;
-// let b = 22;
-// console.log(a % b);
--- jsbasics/loop.js
+++
@@ -1,5 +0,0 @@
-let roleson = "i am roleson";
-let x = ['sdf', 'sdfs'];
-for (let i = 0; i < Array.length + 1; i++) {
-    console.log(x[i]);
-}
